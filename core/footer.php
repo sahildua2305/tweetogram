ca5455ec4d3d04ef30b6f7117265577ab85e85ff
Sahil <sahildua2305@gmail.com>
2014-04-14 05:02:18 +0530

    added twitter links in footer

--- core/footer.php
+++ core/footer.php
@@ -1,6 +1,14 @@
+		<hr>
+		
 		<a href="https://twitter.com/sahildua2305" class="twitter-follow-button" data-show-count="false" data-lang="en" data-size="large" >Follow @twitter</a>
 		
-		<a href="https://twitter.com/share" class="twitter-share-button" data-lang="en" data-url="http://sahildua.collegespace.in/tweetogram/" data-size="large" data-via="sahildua2305" data-hashtags="tweetogram,twitterAPI" data-lang="en" data-text="Tweetogram | The coolest way to learn the scope of Twitter API">Tweet</a>
+		<a href="https://twitter.com/share" class="twitter-share-button" data-lang="en" data-url="http://sahildua.collegespace.in/tweetogram/" data-size="large" data-via="sahildua2305" data-hashtags="Tweetogram,TwitterAPI" data-lang="en" data-text="Tweetogram | The coolest way to learn the scope of Twitter API" data-related="sahildua2305:Hard-working guy behind Tweetogram">Tweet</a>
+		
+		<a href="https://twitter.com/intent/tweet?button_hashtag=Tweetogram" class="twitter-hashtag-button" data-hashtags="TwitterAPI" data-lang="en" data-related="sahildua2305:Hard-working guy behind Tweetogram" data-size="large">Tweet #Tweetogram</a>
+		
+		<a href="https://twitter.com/intent/tweet?screen_name=sahildua2305" class="twitter-mention-button" data-lang="en" data-size="large" data-related="sahildua2305:Hard-working guy behind Tweetogram">Tweet to @twitterapi</a>
+		
+		<hr>
 		
 		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 
