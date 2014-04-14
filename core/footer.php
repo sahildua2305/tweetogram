a64e2bf5d388743dd542882faaf430c69064d600
Sahil <sahildua2305@gmail.com>
2014-04-14 23:53:05 +0530

    adding social buttons

--- core/footer.php
+++ core/footer.php
@@ -1,13 +1,18 @@
 		<hr>
 		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 		
-		<a href="https://twitter.com/sahildua2305" class="twitter-follow-button" data-show-count="false" data-lang="en" data-size="large" >Follow @twitter</a>
+		<table class="table table-striped"><tr><td><a href="https://twitter.com/sahildua2305" class="twitter-follow-button" data-show-count="false" data-lang="en" data-size="large" >Follow @twitter</a></td>
+		<td><a href="https://twitter.com/share" class="twitter-share-button" data-lang="en" data-url="http://sahildua.collegespace.in/tweetogram/" data-size="large" data-via="sahildua2305" data-hashtags="Tweetogram,TwitterAPI" data-lang="en" data-text="Tweetogram | The coolest way to learn the scope of Twitter API" data-related="sahildua2305:Hard-working guy behind Tweetogram">Tweet</a></td>
+		<td><a href="https://twitter.com/intent/tweet?button_hashtag=Tweetogram" class="twitter-hashtag-button" data-hashtags="TwitterAPI" data-lang="en" data-related="sahildua2305:Hard-working guy behind Tweetogram" data-size="large">Tweet #Tweetogram</a></td>
+		<td><a href="https://twitter.com/intent/tweet?screen_name=sahildua2305" class="twitter-mention-button" data-lang="en" data-size="large" data-related="sahildua2305:Hard-working guy behind Tweetogram">Tweet to @twitterapi</a></td></tr></table>
 		
-		<a href="https://twitter.com/share" class="twitter-share-button" data-lang="en" data-url="http://sahildua.collegespace.in/tweetogram/" data-size="large" data-via="sahildua2305" data-hashtags="Tweetogram,TwitterAPI" data-lang="en" data-text="Tweetogram | The coolest way to learn the scope of Twitter API" data-related="sahildua2305:Hard-working guy behind Tweetogram">Tweet</a>
+		<hr>
 		
-		<a href="https://twitter.com/intent/tweet?button_hashtag=Tweetogram" class="twitter-hashtag-button" data-hashtags="TwitterAPI" data-lang="en" data-related="sahildua2305:Hard-working guy behind Tweetogram" data-size="large">Tweet #Tweetogram</a>
-		
-		<a href="https://twitter.com/intent/tweet?screen_name=sahildua2305" class="twitter-mention-button" data-lang="en" data-size="large" data-related="sahildua2305:Hard-working guy behind Tweetogram">Tweet to @twitterapi</a>
+		<table><tr><td><iframe src="http://ghbtns.com/github-btn.html?user=sahildua2305&repo=tweetogram&type=watch&count=true" height="50" width="118" frameborder="0" scrolling="0" style="width:118px; height: 30px;" allowTransparency="true"></iframe></td>
+		<td><iframe src="http://ghbtns.com/github-btn.html?user=sahildua2305&repo=tweetogram&type=fork&count=true" height="50" width="118" frameborder="0" scrolling="0" style="width:118px; height: 30px;" allowTransparency="true"></iframe></td>
+		<td><iframe src="http://ghbtns.com/github-btn.html?user=sahildua2305&type=follow" height="50" width="168" frameborder="0" scrolling="0" style="width:168px; height: 30px;" allowTransparency="true"></iframe></td>
+		<td><div class="g-plusone" data-size="medium"   data-href="http://sahildua.collegespace.in/tweetogram/"></div><script type="text/javascript">(function() {var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;po.src = 'https://apis.google.com/js/plusone.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);})();</script></td><td><script src="//platform.linkedin.com/in.js" type="text/javascript"> lang: en_US </script>
+		<script type="IN/Share" data-url="http://sahildua.collegespace.in/tweetogram/"></script></td><td><iframe src="//www.facebook.com/plugins/like.php?href=http://sahildua.collegespace.in/tweetogram/&send=false&layout=button_count&width=120&show_faces=true&font&colorscheme=light&action=like&height=20" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:120px; height:20px;" allowTransparency="true"></iframe></td></tr></table>
 		
 		<hr>
 		
